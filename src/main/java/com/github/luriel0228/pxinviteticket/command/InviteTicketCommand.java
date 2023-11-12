9559821468cd9532b73eb9b85d74bbf3f0ab454e
Luriel0228 <jnk1io@naver.com>
2023-11-12 22:08:39 +0900

    오류 수정, 코드 업데이트, 개발 완료

--- src/main/java/com/github/luriel0228/pxinviteticket/command/InviteTicketCommand.java
+++ src/main/java/com/github/luriel0228/pxinviteticket/command/InviteTicketCommand.java
@@ -1,55 +1,53 @@
 package com.github.luriel0228.pxinviteticket.command;
 
-import com.github.luriel0228.pxinviteticket.PXInviteTicket;
 import com.github.luriel0228.pxinviteticket.message.Message;
-import com.github.luriel0228.pxinviteticket.message.MessageConfig;
 import com.github.luriel0228.pxinviteticket.message.MessageKey;
 import com.github.luriel0228.pxinviteticket.valid.InvitedValid;
 import com.github.luriel0228.pxinviteticket.valid.PermissionValid;
-
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.logging.Level;
 
 public class InviteTicketCommand implements CommandExecutor {
 
     private final JavaPlugin plugin;
     private final InvitedValid invitedValid;
-    private FileConfiguration config;
-    private final Message msgData = Message.getInstance();
+    private final File settingFile;
+    private final FileConfiguration settingConfig;
 
-    public InviteTicketCommand(PXInviteTicket plugin, InvitedValid invitedValid) {
+    public InviteTicketCommand(JavaPlugin plugin, InvitedValid invitedValid) {
         this.plugin = plugin;
         this.invitedValid = invitedValid;
+        this.settingFile = new File(plugin.getDataFolder(), "setting.yml");
+        this.settingConfig = YamlConfiguration.loadConfiguration(settingFile);
     }
 
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
 
-        if (!(sender instanceof Player)) {
-            sender.sendMessage(msgData.getMessage(MessageKey.PLAYER_ONLY));
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage(Message.getInstance().getMessage(MessageKey.PLAYER_ONLY));
             return true;
         }
 
-        Player player = (Player) sender;
-
         if (args.length == 0) {
-            player.sendMessage(msgData.getMessage(MessageKey.WRONG_COMMAND));
+            player.sendMessage(Message.getInstance().getMessage(MessageKey.WRONG_COMMAND));
             return true;
         }
 
@@ -71,11 +69,11 @@ public class InviteTicketCommand implements CommandExecutor {
                     handleGiveInviteCommand(player, args);
                     break;
                 default:
-                    player.sendMessage(msgData.getMessage(MessageKey.WRONG_COMMAND));
+                    player.sendMessage(Message.getInstance().getMessage(MessageKey.WRONG_COMMAND));
                     break;
             }
         } catch (SQLException e) {
-            player.sendMessage(msgData.getMessage(MessageKey.SQL_ERROR));
+            player.sendMessage(Message.getInstance().getMessage(MessageKey.SQL_ERROR));
             plugin.getLogger().log(Level.SEVERE, "An SQL exception occurred.", e);
         }
 
@@ -85,57 +83,54 @@ public class InviteTicketCommand implements CommandExecutor {
     private void handleReloadCommand(Player player) {
         if (PermissionValid.hasPermission(player, "reload")) {
             plugin.reloadConfig();
-            MessageConfig.reload();
-            player.sendMessage(msgData.getMessage(MessageKey.RELOAD_CONFIG));
+            player.sendMessage(Message.getInstance().getMessage(MessageKey.RELOAD_CONFIG));
         }
     }
 
-    private void handleRegisterCommand(Player player, String @NotNull [] args) throws SQLException {
-        config = plugin.getConfig();
-
+    private void handleRegisterCommand(Player player, String[] args) throws SQLException {
         if (args.length > 1) {
             String invitedPlayerName = args[1];
 
             if (invitedPlayerName.isEmpty()) {
-                player.sendMessage(msgData.getMessage(MessageKey.MISSING_PLAYER));
+                player.sendMessage(Message.getInstance().getMessage(MessageKey.MISSING_PLAYER));
                 return;
             }
 
             ItemStack inviteItem = getInviteItem();
             if (!player.getInventory().containsAtLeast(inviteItem, 1)) {
-                player.sendMessage(msgData.getMessage(MessageKey.NO_INVITE_ITEM));
+                player.sendMessage(Message.getInstance().getMessage(MessageKey.NO_INVITE_ITEM));
                 return;
             }
 
             if (invitedPlayerName.equalsIgnoreCase(player.getName())) {
-                player.sendMessage(msgData.getMessage(MessageKey.SELF_INVITE));
+                player.sendMessage(Message.getInstance().getMessage(MessageKey.SELF_INVITE));
                 return;
             }
 
             if (invitedValid.isInvitedPlayer(invitedPlayerName)) {
-                String message = msgData.getMessage(MessageKey.ALREADY_INVITED);
+                String message = Message.getInstance().getMessage(MessageKey.ALREADY_INVITED);
                 String formattedMessage = message.replace("{player}", invitedPlayerName);
                 player.sendMessage(formattedMessage);
                 return;
             }
 
-            int inviteLimit = config.getInt("InviteSetting.InviteLimit");
+            int inviteLimit = plugin.getConfig().getInt("InviteSetting.InviteLimit");
             int playerInvitedCount = invitedValid.getInvitesCount(player.getName());
 
             if (playerInvitedCount >= inviteLimit) {
-                player.sendMessage(msgData.getMessage(MessageKey.MAX_INVITES_REACHED));
+                player.sendMessage(Message.getInstance().getMessage(MessageKey.MAX_INVITES_REACHED));
                 return;
             }
 
             invitedValid.registerInvite(player.getName(), invitedPlayerName);
             updateInviteCount(player.getName());
             player.getInventory().removeItem(inviteItem);
-            String message = msgData.getMessage(MessageKey.SET_INVITE_SUCCESS);
+            String message = Message.getInstance().getMessage(MessageKey.SET_INVITE_SUCCESS);
             String formattedMessage = message.replace("{player}", invitedPlayerName);
             player.sendMessage(formattedMessage);
 
         } else {
-            player.sendMessage(msgData.getMessage(MessageKey.MISSING_PLAYER));
+            player.sendMessage(Message.getInstance().getMessage(MessageKey.MISSING_PLAYER));
         }
     }
 
@@ -143,18 +138,20 @@ public class InviteTicketCommand implements CommandExecutor {
         ItemStack itemInHand = player.getInventory().getItemInMainHand();
         if (PermissionValid.hasPermission(player, "admin")) {
             if (itemInHand.getType() == Material.AIR) {
-                player.sendMessage(msgData.getMessage(MessageKey.EMPTY_HAND));
+                player.sendMessage(Message.getInstance().getMessage(MessageKey.EMPTY_HAND));
                 return;
             }
 
             saveInviteItem(itemInHand);
-            player.sendMessage(msgData.getMessage(MessageKey.INVITE_ITEM_REGISTERED));
+            setInviteItemInHand(itemInHand);
+            player.sendMessage(Message.getInstance().getMessage(MessageKey.INVITE_ITEM_REGISTERED));
         }
     }
 
-    private void saveInviteItem(ItemStack inviteItem) {
-        ConfigurationSection configSection = getConfigSection();
+    private void setInviteItemInHand(ItemStack inviteItem) {
+        ConfigurationSection configSection = getSettingSection();
 
+        // 초대권 정보 갱신
         configSection.set("material", inviteItem.getType().name());
 
         ItemMeta itemMeta = inviteItem.getItemMeta();
@@ -166,18 +163,34 @@ public class InviteTicketCommand implements CommandExecutor {
             configSection.set("lore", itemMeta.getLore());
         }
 
-        plugin.saveConfig();
+        saveSettingConfig();
     }
 
-    private ConfigurationSection getConfigSection() {
-        ConfigurationSection configSection = plugin.getConfig().getConfigurationSection("InviteSetting.InviteItem");
-        if (configSection == null) {
-            configSection = plugin.getConfig().createSection("InviteSetting.InviteItem");
+
+    private void saveInviteItem(ItemStack inviteItem) {
+        ConfigurationSection configSection = getSettingSection();
+
+        if (configSection.get("material") != null) {
+            return;
         }
-        return configSection;
+
+        configSection.set("material", inviteItem.getType().name());
+        ItemMeta itemMeta = inviteItem.getItemMeta();
+        if (itemMeta != null && itemMeta.hasDisplayName()) {
+            configSection.set("name", itemMeta.getDisplayName());
+        }
+        if (itemMeta != null && itemMeta.hasLore()) {
+            configSection.set("lore", itemMeta.getLore());
+        }
+
+        saveSettingConfig();
     }
 
-    private void handleGiveInviteCommand(Player player, String @NotNull [] args) {
+    private ItemStack getInviteItem() {
+        return loadCustomInviteItem();
+    }
+
+    private void handleGiveInviteCommand(Player player, String[] args) {
         if (PermissionValid.hasPermission(player, "admin")) {
             String targetPlayerName;
 
@@ -187,72 +200,32 @@ public class InviteTicketCommand implements CommandExecutor {
                 targetPlayerName = player.getName();
             }
 
-            ItemStack inviteItem = loadInviteItem();
+            ItemStack inviteItem = loadCustomInviteItem();
             Player targetPlayer = plugin.getServer().getPlayerExact(targetPlayerName);
 
             if (targetPlayer != null) {
                 targetPlayer.getInventory().addItem(inviteItem);
-                String message = msgData.getMessage(MessageKey.GIVE_INVITE_SUCCESS);
+                String message = Message.getInstance().getMessage(MessageKey.GIVE_INVITE_SUCCESS);
                 String formattedMessage = message.replace("{player}", targetPlayerName);
                 player.sendMessage(formattedMessage);
             } else {
-                player.sendMessage(msgData.getMessage(MessageKey.INVALID_PLAYER));
+                player.sendMessage(Message.getInstance().getMessage(MessageKey.INVALID_PLAYER));
             }
         } else {
-            player.sendMessage(msgData.getMessage(MessageKey.NO_PERMISSION));
+            player.sendMessage(Message.getInstance().getMessage(MessageKey.NO_PERMISSION));
         }
     }
 
-    private ItemStack loadInviteItem() {
-        ConfigurationSection configSection = getConfigSection();
-        Material material = Material.matchMaterial(configSection.getString("material", "PAPER"));
-
-        if (material == null) {
-            plugin.getLogger().warning(msgData.getMessage(MessageKey.NO_INVITE_ITEM_SET));
-            return createDefaultInviteItem();
-        }
-
-        ItemStack inviteItem = new ItemStack(material);
-        ItemMeta itemMeta = inviteItem.getItemMeta();
-
-        if (configSection.contains("name")) {
-            Objects.requireNonNull(itemMeta).setDisplayName(configSection.getString("name"));
-        }
-
-        if (configSection.contains("lore")) {
-            List<String> lore = configSection.getStringList("lore");
-            Objects.requireNonNull(itemMeta).setLore(lore);
-        }
-
-        inviteItem.setItemMeta(itemMeta);
-        return inviteItem;
-    }
-
-    private ItemStack createDefaultInviteItem() {
-        ItemStack defaultInviteItem = new ItemStack(Material.PAPER);
-        ItemMeta itemMeta = defaultInviteItem.getItemMeta();
-        if (itemMeta != null) {
-            itemMeta.setDisplayName("초대권");
-            defaultInviteItem.setItemMeta(itemMeta);
-        }
-        return defaultInviteItem;
-    }
-
-    private ItemStack getInviteItem() {
-        return loadInviteItem();
-    }
-
-    private void handleListCommand(@NotNull Player player) throws SQLException {
-        this.config = plugin.getConfig();
+    private void handleListCommand(Player player) throws SQLException {
         List<String> invitedUsers = getInvitedUsers(player.getName());
         if (invitedUsers.isEmpty()) {
-            player.sendMessage(msgData.getMessage(MessageKey.NO_INVITED_PLAYERS));
+            player.sendMessage(Message.getInstance().getMessage(MessageKey.NO_INVITED_PLAYERS));
             return;
         }
 
-        int inviteLimit = config.getInt("InviteSetting.InviteLimit");
+        int inviteLimit = plugin.getConfig().getInt("InviteSetting.InviteLimit");
         int playerInvitedCount = invitedValid.getInvitesCount(player.getName());
-        String message = msgData.getMessage(MessageKey.INVITED_PLAYER);
+        String message = Message.getInstance().getMessage(MessageKey.INVITED_PLAYER);
         String formattedMessage = message.replace("{int}", String.valueOf(playerInvitedCount))
                 .replace("{max}", String.valueOf(inviteLimit));
         player.sendMessage(formattedMessage);
@@ -264,13 +237,63 @@ public class InviteTicketCommand implements CommandExecutor {
         invitedValid.updateInviteCount(inviter);
     }
 
-    private @NotNull List<String> getInvitedUsers(String inviterName) {
-        List<String> invitedUsers = new ArrayList<>();
-        for (Map.Entry<String, String> entry : invitedValid.getInvites().entrySet()) {
-            if (entry.getValue().equals(inviterName)) {
-                invitedUsers.add(entry.getKey());
+    private List<String> getInvitedUsers(String inviterName) {
+        return invitedValid.getInvites().entrySet().stream()
+                .filter(entry -> entry.getValue().equals(inviterName))
+                .map(Map.Entry::getKey)
+                .toList();
+    }
+
+    private ConfigurationSection getSettingSection() {
+        ConfigurationSection settingSection = settingConfig.getConfigurationSection("InviteSetting");
+        if (settingSection == null) {
+            // setting.yml에 InviteSetting 섹션이 없으면 생성
+            settingSection = settingConfig.createSection("InviteSetting");
+        }
+
+        ConfigurationSection inviteItemSection = settingSection.getConfigurationSection("InviteItem");
+        if (inviteItemSection == null) {
+            // setting.yml에 InviteItem 섹션이 없으면 생성
+            inviteItemSection = settingSection.createSection("InviteItem");
+        }
+
+        return inviteItemSection;
+    }
+
+    private ItemStack loadCustomInviteItem() {
+        ConfigurationSection configSection = getSettingSection();
+        Material material = Material.matchMaterial(configSection.getString("material", "PAPER"));
+
+        if (material == null) {
+            plugin.getLogger().warning("No valid material set for invite item.");
+            return null;
+        }
+
+        ItemStack inviteItem = new ItemStack(material);
+        ItemMeta itemMeta = inviteItem.getItemMeta();
+
+        if (configSection.contains("name")) {
+            if (itemMeta != null) {
+                itemMeta.setDisplayName(configSection.getString("name"));
             }
         }
-        return invitedUsers;
+
+        if (configSection.contains("lore")) {
+            if (itemMeta != null) {
+                List<String> lore = configSection.getStringList("lore");
+                itemMeta.setLore(lore);
+            }
+        }
+
+        inviteItem.setItemMeta(itemMeta);
+        return inviteItem;
+    }
+
+    private void saveSettingConfig() {
+        try {
+            settingConfig.save(settingFile);
+        } catch (IOException e) {
+            plugin.getLogger().log(Level.SEVERE, "Could not save setting.yml", e);
+        }
     }
 }
